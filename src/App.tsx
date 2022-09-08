2342a3ef27e6e12f28d41a658c2714d7709085bf
Alex Paltinean <alex.paltinean@appointfix.com>
2022-09-08 22:32:46 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Country } from './model/Country';
+import { Button, Card, CardContent, CardHeader, Checkbox, Chip, FormControlLabel, Grid, Typography } from '@mui/material';
 
-function App() {
+const App: FC = () => {
+  // code, logic...
+  const [countries, setCountries] = useState<Country[]>();
+  const [selectedCountry, setSelectedCountry] = useState<Country | undefined>(undefined);
+
+  const onClick = (country: Country) => {
+    console.log(country.name + " was clicked");
+    setSelectedCountry(country);
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:8888/countries').then((response) => setCountries(response.data));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {selectedCountry && <Card sx={{ margin: 2, backgroundColor:"gray" }}>
+        <CardContent>{selectedCountry?.name} will be edited here</CardContent>
+        <Button onClick={()=>setSelectedCountry(undefined)}>Close</Button>
+      </Card>}
+      {countries?.map(country =>
+        <Card sx={{ margin: 2 }}>
+          <CardContent>
+            <Typography>{country.name}</Typography>
+            <Chip label={country?.capital.name}></Chip>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Typography fontSize="small" sx={{ color: 'gray' }}>Continent: {country.continent}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography fontSize="small">Area: {country.area}</Typography>
+              </Grid>
+            </Grid>
+            <FormControlLabel control={<Checkbox checked={country.continent === "Europe"} disabled />} label="In Europe" />
+            <Button variant="contained" onClick={() => onClick(country)}>Edit</Button>
+          </CardContent>
+        </Card>)}
     </div>
   );
 }
---
+++ src/model/Country.ts
@@ -0,0 +1,9 @@
+import { City } from "./City";
+export interface Country {
+    id: number;
+    name: string;
+    population: number;
+    area: number;
+    continent: string;
+    capital: City;
+}
